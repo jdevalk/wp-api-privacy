c7a2bd6d07b750da21ea94d3f02290b13833d7ef
Duane Storey <duanestorey@gmail.com>
2024-11-07 09:57:02 -0800

    Tagging version 4.0

--- wp-api-privacy.php
+++ wp-api-privacy.php
@@ -6,17 +6,17 @@
     Description: Strips potentially identifying information from outbound requests to the WordPress.org API
     Author: Duane Storey
     Author URI: https://duanestorey.com
-    Version: 0.0.3
+    Version: 0.0.4
     Requires PHP: 6.0
     Requires at least: 6.0
     Tested up to: 6.6
     Update URI: https://github.com/wpprivacy/wp-api-privacy
-    Stable: 0.0.3
+    Stable: 0.0.4
 */
 
 namespace WP_Privacy\WP_API_Privacy;
 
-define( 'PRIVACY_VERSION', '0.0.1' );
+define( 'PRIVACY_VERSION', '0.0.4' );
 
 require_once( dirname( __FILE__ ) . '/src/api-privacy.php' );
 
