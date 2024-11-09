44a982e6b53ab1a660ccf235a27170de0a1e4a14
Duane Storey <duanestorey@gmail.com>
2024-11-08 16:23:53 -0800

    Updated URLs

--- wp-api-privacy.php
+++ wp-api-privacy.php
@@ -1,7 +1,7 @@
 <?php
 /*
     Plugin Name: WP API Privacy
-    Plugin URI: https://github.com/wpprivacy/wp-api-privacy
+    Plugin URI: https://github.com/wp-privacy/wp-api-privacy
     Banner: https://github.com/wp-privacy/wp-api-privacy/blob/main/assets/banner.jpg?raw=true
     Description: Strips potentially identifying information from outbound requests to the WordPress.org API
     Author: Duane Storey
@@ -10,7 +10,7 @@
     Requires PHP: 6.0
     Requires at least: 6.0
     Tested up to: 6.7
-    Update URI: https://github.com/wpprivacy/wp-api-privacy
+    Update URI: https://github.com/wp-privacy/wp-api-privacy
     Stable: 1.0.3
     Text Domain: wp-api-privacy
     Domain Path: /lang
